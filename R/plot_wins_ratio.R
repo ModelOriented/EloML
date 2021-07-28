c1681ead229d6aa2967374030344f21c2493b63c
woznicak <kaswoz4@gmail.com>
2021-07-28 22:56:38 +0200

    random sample in plot_wins_ratio #12

--- R/plot_wins_ratio.R
+++ R/plot_wins_ratio.R
@@ -4,6 +4,8 @@
 #' estimated from EPP values.
 #'
 #' @param epp epp_results. The result of a function \code{\link{calculate_epp}}.
+#' @param random_sample Numeric. If not NULL `random sample` fraction of Players is sampled from the set of all Players.
+#' @param random_state Numeric. Seed for sampling Players. Used only when `random_sample` is not NULL.
 #'
 #' @return gg
 #'
@@ -22,11 +24,27 @@
 #'
 #' @import ggplot2
 #' @export
-plot_wins_ratio <- function(epp){
+plot_wins_ratio <- function(epp, random_sample = NULL, random_state = NULL){
   ratio <- pred_ratio <- NULL
   if(!("actual_score" %in% names(epp))) stop("Use `keep_data == TRUE` parameter in function `calculate_epp()`")
   actual_score <- epp$actual_score
   epp_score <- epp$epp
+
+
+  if(!is.null(random_sample)){
+
+    if(!is.numeric(random_sample)) stop("Use `random_sample` as numeric value in (0,1].")
+    if(random_sample>1 | random_sample <= 0) stop("Use `random_sample` as numeric value in (0,1].")
+
+    if(!is.null(random_state)){
+      if(!is.numeric(random_state)) stop("Use `seed` as numeric value.")
+      set.seed(random_state)
+    }
+
+    sample_player <- sample(epp_score$model, size = floor(random_sample * length(epp_score$model)))
+    actual_score <- actual_score[actual_score$winner %in% sample_player & actual_score$loser %in% sample_player,]
+    epp_score <- epp_score[epp_score$model %in% sample_player, ]
+  }
 
   actual_score[["ratio"]] <- actual_score[["wins"]] / actual_score[["match"]]
   actual_score <- merge(actual_score, epp_score, by.x ="winner", by.y = "model")
